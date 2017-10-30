50ad6aab845373467c27e64bf841146349742c88
vagrant <vagrant@vagrant.vm>
2017-10-30 03:02:09 +0000

    add redirect_uri in client_secrets.json, add state parameter in return method of login(), bugs cleaned for redirecting.

--- project.py
+++ project.py
@@ -27,7 +27,7 @@ def login():
     state = ''.join(random.choice(string.ascii_uppercase 
              + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html')
+    return render_template('login.html', STATE=state)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
